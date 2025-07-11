8513932444e7f6346005aa0c2d44d67b27b55e88
Ömer Faruk Çift <omerfciftt@gmail.com>
2025-07-11 05:12:57 +0300

    Update watchdog_setup.ino

--- Main Algorithm/watchdog_setup.ino
+++ Main Algorithm/watchdog_setup.ino
@@ -1,24 +1,3 @@
-/*
-Interrupt Watchdog (IWDT)
-RTC Watchdog Timer (RTC_WDT)
-
-ESP32 mimarisinde iki önemli sistem watchdog’u olan Interrupt Watchdog (IWDT) ve RTC Watchdog Timer (RTC_WDT), 
-uygulama koduna herhangi bir müdahaleye gerek kalmadan sistem seviyesinde otomatik olarak çalışmaktadır.
-IWDT, FreeRTOS işletim sistemi tarafından her tick’te beslenerek, ISR’ların veya kritik bölümlerin sistemin kilitlenmesine neden olup olmadığını denetler. 
-RTC_WDT ise, sistem açıldığında (boot aşamasında) devreye girerek, main() fonksiyonuna zamanında ulaşılamazsa otomatik reset işlemi gerçekleştirir. 
-Bu iki watchdog, ESP32 sisteminin kararlı çalışmasını sağlamakta, olası kilitlenmelerde sistemin kendi kendine toparlanmasına imkân tanımaktadır.
-
-Task Watchdog Timer (TWDT)
-
-ESP32 platformunda kullanılan Task Watchdog Timer (TWDT), belirli görevlerin (task'ların) belirli bir süre içerisinde düzgün şekilde 
-çalışıp çalışmadığını denetlemek amacıyla kullanılan bir yazılımsal koruma mekanizmasıdır. 
-Kullanıcı tarafından başlatılarak aktif hâle getirilen TWDT’ye, izlenmesi istenen görevler abone edilir. 
-Bu görevler, belirlenen zaman aralığında esp_task_wdt_reset() fonksiyonu ile watchdog'u “beslemeli”; aksi takdirde 
-TWDT bu görevin kilitlendiğini varsayarak sistemin hata mesajı üretmesini (panic) ya da resetlenmesini sağlar.
-Roket sistemleri gibi güvenlik-kritik uygulamalarda TWDT, sistemin ana görevlerinin donması, veri akışının kesilmesi veya 
-infinite loop gibi yazılım hatalarının oluşması durumunda müdahale ederek sistemin kendi kendini kurtarabilmesini sağlar.
-*/
-
 #include "esp_task_wdt.h"
 
 void setup() {
